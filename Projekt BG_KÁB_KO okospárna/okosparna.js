c585f7dc6af03e0629ae3f2fd3674d273ca44c59
kondoraron <162179003+kondoraron@users.noreply.github.com>
2026-03-09 20:29:27 +0100

    Add files via upload

---
+++ Projekt BG_KÁB_KO okospárna/okosparna.js
@@ -0,0 +1,75 @@
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("open");
+});
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+let hero = document.querySelector(".hero");
+
+let scroll = window.scrollY;
+
+hero.style.opacity = 1 - scroll / 450;
+
+});
+
+
+
+
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach(section => {
+
+const sectionTop = section.offsetTop - 100;
+const sectionHeight = section.clientHeight;
+
+if(pageYOffset >= sectionTop){
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach(link => {
+
+link.classList.remove("active");
+
+if(link.getAttribute("href") == "#" + current){
+link.classList.add("active");
+}
+
+});
+
+});
+
+
+
+
+
+let slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+setInterval(nextSlide, 3000);
